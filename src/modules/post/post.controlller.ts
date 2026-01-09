3bcc3a423169f9b555813669d2d55ecb40135288
Ayon203008 <paypal572874@gmail.com>
2026-01-09 22:51:20 +0600

    better auth added successfulyy

--- src/modules/post/post.controlller.ts
+++ src/modules/post/post.controlller.ts
@@ -5,7 +5,7 @@ import { postService } from "./post.service"
 const createPost = async (req: Request, res: Response) => {
     try {
         const result = await postService.createPost(req.body)
-        res.send(201).json(result)
+        res.status(201).json(result)
     } catch (err) {
         console.log(err)
     }
